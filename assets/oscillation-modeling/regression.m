89eba60a6c2b013fb27a003cfc9df20b2f232fb0
Adam Zhang <898544@lcps.org>
2024-05-02 16:27:05 -0400

    oscillation: use nonlin_curvefit for octave regression
    So I can set bounds later™.

--- assets/oscillation-modeling/regression.m
+++ assets/oscillation-modeling/regression.m
@@ -16,16 +16,16 @@ function beta = perform_regression (column_idx, beta0)
     Y = [Y; data(2:end, column_idx + (i * 2) + 1)];
   endfor
   
-  [beta, R, _, _] = nlinfit(X, Y, model, beta0);
+  [beta, R, _, _] = nonlin_curvefit(model, beta0, X, Y);
 endfunction
 
 ## Control
-control_p = perform_regression(1, [0.045, 7.3, 0.001]);
+control_p = perform_regression(1, [0.045; 7.3; 0.001]);
 ## Initial position
-pos_p = perform_regression(7, [0.07, 7.3, 0.001]);
+pos_p = perform_regression(7, [0.07; 7.3; 0.001]);
 ## Mass
-m_p = perform_regression(13, [0.05, 5.2, 0.001]);
+m_p = perform_regression(13, [0.05; 5.2; 0.001]);
 ## Spring
-k_p = perform_regression(19, [0.05, 7.7, 0.001]);
+k_p = perform_regression(19, [0.05; 7.7; 0.001]);
 
 csvwrite("parameters.csv", [control_p'; pos_p'; m_p'; k_p']);
