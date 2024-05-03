2aecf4505e74c6b7a3cb6b98e9e01c9cb5b38c5d
Adam Zhang <898544@lcps.org>
2024-05-03 12:48:30 -0400

    oscillation: do a regression for every trial

--- assets/oscillation-modeling/regressions.m
+++ assets/oscillation-modeling/regressions.m
@@ -10,25 +10,19 @@ function beta = perform_regression (column_idx, beta0)
   global data model lbound;
 
   # Combine data from all three trials
-  X = [];
-  Y = [];
-  for i = 0:2
-    X = [X; data(2:end, column_idx + (i * 2))];
-    Y = [Y; data(2:end, column_idx + (i * 2) + 1)];
-  endfor
+  X = data(2:end, column_idx);
+  Y = data(2:end, column_idx);
 
   options = optimset("lbound", lbound);
   
   [beta, R, _, _] = nonlin_curvefit(model, beta0, X, Y, options);
 endfunction
 
-## Control
-control_p = perform_regression(1, [0.045; 7.3; 0.001]);
-## Initial position
-pos_p = perform_regression(7, [0.07; 7.3; 0.001]);
-## Mass
-m_p = perform_regression(13, [0.05; 5.2; 0.001]);
-## Spring
-k_p = perform_regression(19, [0.05; 7.7; 0.001]);
+result = [];
 
-csvwrite("parameters.csv", [control_p'; pos_p'; m_p'; k_p']);
+for i = 1:12
+  params = perform_regression(i * 2, [0.045; 7.3; 0.001]);
+  result = [result; params'];
+endfor
+
+csvwrite("parameters.csv", result);
