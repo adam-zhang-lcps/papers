157cba3b231be7e5338294f16726484e8057feec
Adam Zhang <898544@lcps.org>
2024-05-03 22:59:41 -0400

    oscillation: fix initial position regression

--- assets/oscillation-modeling/regressions.m
+++ assets/oscillation-modeling/regressions.m
@@ -4,20 +4,29 @@ pkg load optim;
 
 global data = csvread("data.csv");
 global model = @(b, X) b(1) * cos(b(2) * X + b(3));
+global bounds = [0; -inf; -inf];
 
 function beta = perform_regression (column_idx, beta0)
-  global data model;
+  global data model bounds;
 
   X = data(2:end, 1);
   Y = data(2:end, 1 + column_idx);
 
-  [beta, R, _, _] = nonlin_curvefit(model, beta0, X, Y);
+  options = optimset("lbound", bounds);
+
+  [beta, R, _, _] = nonlin_curvefit(model, beta0, X, Y, options);
 endfunction
 
 result = [];
 
 for i = 1:12
   beta0 = [0.045; 7.3; 0.001];
+
+  ## Trials with a further initial position have inverted position. Why? I have
+  ## no idea.
+  if i > 3 && i < 7
+    beta0 = [0.07; 7.3; -3];
+  endif
 
   ## Trials with a heavier mass have a significantly lower period that optimization
   ## isn't able to find from the usual starting point.
