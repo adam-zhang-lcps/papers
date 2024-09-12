e18e99d4aeb9f8d32ee7c193ed2dba3ae4604b23
Adam Zhang <898544@lcps.org>
2024-09-12 14:17:53 -0400

    tools: set eglot-workspace-configuration in dir-locals
    For some reason, it's impossible to set this via initialization options 😕.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((typst-ts-mode . ((eglot-workspace-configuration . (:exportPdf "onType")))))
